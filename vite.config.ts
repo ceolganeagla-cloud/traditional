f1a55a5b12eab0e318b7b22a257f14e2e3914c04
ceolganeagla-cloud <ceolganeagla@gmail.com>
2025-10-15 00:03:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins:[react()], base:'' })
+
+export default defineConfig({
+  plugins: [react()],
+  // IMPORTANT: this must match the repo name used on Pages
+  base: '/traditional/',
+})
