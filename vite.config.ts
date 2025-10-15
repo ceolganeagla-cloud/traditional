358175fc386efa3aab3ffe205a99ca6936ced5ec
ceolganeagla-cloud <ceolganeagla@gmail.com>
2025-10-15 13:16:41 +0100

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // IMPORTANT: this must match the repo name used on Pages
-  base: '/traditional/',
-})
